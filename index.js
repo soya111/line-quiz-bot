0cc5a24039a6b50c96fb6ee7b665cc49117fbabf
soya111 <soyak20000316@gmail.com>
2020-09-28 01:30:56 +0900

    画像とスタンプに反応させなくした

--- index.js
+++ index.js
@@ -83,10 +83,12 @@ function handleEvent(event) {
         case "text":
           return handleText(message, event.replyToken, event.source);
         case "image":
-          return replyText(event.replyToken, "画像には反応できんよー");
+          return;
+        // return replyText(event.replyToken, "画像には反応できんよー");
         // return handleImage(message, event.replyToken);
         case "video":
-          return replyText(event.replyToken, "動画には反応できんよー");
+          return;
+        // return replyText(event.replyToken, "動画には反応できんよー");
         // return handleVideo(message, event.replyToken);
         case "audio":
           return replyText(event.replyToken, "音声には反応できんよー");
@@ -95,8 +97,9 @@ function handleEvent(event) {
           // return handleLocation(message, event.replyToken);
           return replyText(event.replyToken, "位置情報には反応できんよー");
         case "sticker":
-          // return handleSticker(message, event.replyToken);
-          return replyText(event.replyToken, "スタンプには反応できんよー");
+          return;
+        // return handleSticker(message, event.replyToken);
+        // return replyText(event.replyToken, "スタンプには反応できんよー");
 
         default:
           throw new Error(`Unknown message: ${JSON.stringify(message)}`);
